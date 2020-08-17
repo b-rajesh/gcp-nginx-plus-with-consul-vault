bece7db479d4c1c4aac5c73d897bb26a4b6aa1ab
Rajesh Bavananatham <rajesh.bavan@gmail.com>
2020-08-17 13:07:12 +1000

    Adding .gitignore

--- vpc-network.tf
+++ vpc-network.tf
@@ -33,24 +33,22 @@ resource "google_compute_subnetwork" "microservice-subnet" {
 
 }
 
-# Microservice Subnet
+# Consul Subnet
 resource "google_compute_subnetwork" "consul-sever-subnet" {
   name          = "${random_pet.pet-prefix.id}-${var.consul-subnet}"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = var.consul_subnet_cidr
-
 }
-
-# Microservice Subnet
+/*
+# Vault Subnet
 resource "google_compute_subnetwork" "vault-sever-subnet" {
   name          = "${random_pet.pet-prefix.id}-${var.vault-subnet}"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = var.vault_subnet_cidr
-
 }
-
+*/
 
 resource "google_compute_address" "ext-lb-staticip-address" {
   name = "${random_pet.pet-prefix.id}-ngx-lb-static-ip"
